3dbdd003f02c4e62d72dd1fccf3561c9b7c07d4f
sakamichi <sakamichi9158@gmail.com>
2017-02-21 08:40:11 +0800

    personal setting ui completed

--- app/src/main/java/com/jiaohe/sakamichi/xinzhiying/ui/acitivity/SettingActivity.java
+++ app/src/main/java/com/jiaohe/sakamichi/xinzhiying/ui/acitivity/SettingActivity.java
@@ -2,14 +2,35 @@ package com.jiaohe.sakamichi.xinzhiying.ui.acitivity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 import com.jiaohe.sakamichi.xinzhiying.R;
+import com.jiaohe.sakamichi.xinzhiying.util.UIUtils;
 
-public class SettingActivity extends AppCompatActivity {
+public class SettingActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        UIUtils.initStateBar(SettingActivity.this);
+        initView();
+    }
+
+    private void initView() {
+        ImageButton ib_back = (ImageButton) findViewById(R.id.ib_back);
+
+        ib_back.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ib_back:
+                finish();
+                break;
+        }
     }
 }
